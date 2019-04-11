61cfaecf99be51670a6471c9680884b15073fbc1
Suraj32 <suraj21071998@gmail.com>
2019-04-11 14:55:19 +0530

    Optimised routes related code

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,14 @@
 Rails.application.routes.draw do
-  get 'cycles/index'
-  get 'cycles/new'
-  get 'cycles/create'
-  get 'cycles/update'
-  get 'cycles/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/', to: 'home#index', as: 'intranet', via: :all
   scope path: 'snowflake' do
     get '/get_roles', to: 'uploads#get_roles'
     get '/get_track_categories', to: 'uploads#get_track_categories'
     get '/get_tracks', to: 'uploads#get_tracks'
-    resources :uploads, :roles, :track_categories, :tracks, :levels, :cycles
+    resources :uploads, :roles, :track_categories, :tracks, :levels
+    resources :cycles do
+      resources :assessment_results
+    end
     scope module: 'api' do
       namespace :v1 do
         resources :users
@@ -18,6 +16,6 @@ Rails.application.routes.draw do
         post '/create_assessment', to: 'users#create_assessment'
       end
     end
-    root 'cycles#index'
+    root 'users#new'
   end
 end
